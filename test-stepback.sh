729596848b23cccb8899e0b74c55a13e154440bf
ZackarySantana <zackzackyack@gmail.com>
2023-11-09 11:40:14 -0500

    Add test stepback script

--- test-stepback.sh
+++ test-stepback.sh
@@ -1,3 +1,5 @@
+perl -pe 's/(?<!#)exit 1/#exit 1/g' evergreen.yml > evergreen.yml
+
 git commit --allow-empty -m "1"
 git commit --allow-empty -m "2"
 git commit --allow-empty -m "3"
@@ -8,3 +10,8 @@ git commit --allow-empty -m "7"
 git commit --allow-empty -m "8"
 git commit --allow-empty -m "9"
 git commit --allow-empty -m "10"
+
+perl -pe 's/#exit 1/exit 1/g' evergreen.yml > evergreen.yml
+git commit --allow-empty -m "Failing commit"
+
+perl -pe 's/(?<!#)exit 1/#exit 1/g' evergreen.yml > evergreen.yml
