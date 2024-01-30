3255b11fe97bb1f59a28a1c7619b044b6ad1a7c2
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2024-01-30 16:24:32 -0500

    update script

--- test-stepback-complex.sh
+++ test-stepback-complex.sh
@@ -1,20 +1,20 @@
 perl -i -pe 's/# (exit 1 #1)/$1/g' evergreen.yml # fail 1
-git commit -m "1"
+git commit -am "1"
 perl -i -pe 's/# (exit 1 #2)/$1/g' evergreen.yml # fail 2
-git commit -m "2"
+git commit -am "2"
 git commit --allow-empty -m "3"
 git commit --allow-empty -m "4"
 perl -i -pe 's/# (exit 1 #5)/$1/g' evergreen.yml # fail 5
-git commit -m "5"
+git commit -am "5"
 git commit --allow-empty -m "6"
 perl -i -pe 's/# (exit 1 #7)/$1/g' evergreen.yml # fail 7
-git commit --allow-empty -m "7"
+git commit -am "7"
 perl -i -pe 's/# (exit 1 #8)/$1/g' evergreen.yml # fail 8
-git commit --allow-empty -m "8"
+git commit -am "8"
 git commit --allow-empty -m "9"
 git commit --allow-empty -m "10"
 
-git commit -am "Latest commit"
+git commit --allow-empty -m "Latest commit"
 
 # Reset the file to its original state
 perl -i -pe 's/(?<!# )(exit 1 #1)/# $1/g' evergreen.yml #fail 1
